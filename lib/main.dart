957324c357fe3f054d615166a5ec07966a642257
khushboo jain <khushiejain92@gmail.com>
2024-06-14 20:10:22 +0530

    adds containers and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +9,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false, //removes the debug banner on ui
-     home: Scaffold(), //returns a plain white ui  
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Container( //adds container
+            height: 300,
+            width : 300,
+            color :Colors.green,
+            child:Text("Khushboo Jain"),
+          ),
+        )
+      ),//returns a plain blue ui 
     );
   }
 }
